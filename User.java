4d73aff4e67e04e2f70b13fb7685bffb48b3d346
joe77 <143707322+JonathanAndrianto123@users.noreply.github.com>
2025-06-10 09:47:50 +0700

    mekkk

---
+++ User.java
@@ -0,0 +1,51 @@
+package com.ukdw.prplbo.jackpot;
+
+public class User {
+    private static String username;
+    private static int attempt = 0;
+    private static String status;
+    private static String password;
+
+    public User() {
+    }
+
+    public static String getUsername() {
+        return username;
+    }
+
+    public static int getAttempt() {
+        return attempt;
+    }
+
+    public static String getStatus() {
+        return status;
+    }
+
+    public static String getPassword() {
+        return password;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public static void setUsername(String username) {
+        User.username = username;
+    }
+
+    public void setAttempt(int attempt) {
+        this.attempt = attempt;
+    }
+
+    public static void setPassword(String password) {
+        User.password = password;
+    }
+
+    public int increaseAttempts() {
+        if (status == "Jackpot!!!!!!!!" || status == "Anda mendapat 2 huruf keberuntungan!") {
+            setAttempt(0);
+        }
+        this.attempt += 1;
+        return attempt;
+    }
+}
